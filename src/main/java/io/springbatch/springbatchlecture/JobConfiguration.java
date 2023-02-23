648332262d4ce16af4bcb9f73c5bb7390b0b7f93
kiminjoong <ijboym216@gmail.com>
2023-02-23 23:03:19 +0900

    1. Job

--- src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/JobConfiguration.java
@@ -14,38 +14,39 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class DBJobConfiguration {
+public class JobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job dbJob() {
-        return jobBuilderFactory.get("dbJob")
+    public Job job() {
+        return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
                 .build();
     }
 
-
+    @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step one executed");
+                        System.out.println("step 1 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
                 .build();
+
     }
 
-
+    @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 executed");
+                        System.out.println("step 2 was executed");
                         return RepeatStatus.FINISHED;
                     }
                 })
