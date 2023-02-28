53a136e4076ba9f9727565b22a8d48c3c27fba19
kiminjoong <ijboym216@gmail.com>
2023-02-28 09:50:04 +0900

    1. JobParameter

---
+++ src/main/java/io/springbatch/springbatchlecture/JobParameterConfiguration.java
@@ -0,0 +1,63 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobParameterConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job jobParameterInstance() {
+        return jobBuilderFactory.get("jobParameterInstance")
+                .start(step1())
+                .next(step2())
+                .build();
+    }
+
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        JobParameters jobParameters = stepContribution.getStepExecution().getJobExecution().getJobParameters();
+                        jobParameters.getString("name");
+                        jobParameters.getLong("seq");
+                        jobParameters.getDate("date");
+                        jobParameters.getDouble("age");
+                        System.out.println("step1 executed");
+
+                        Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step2 executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+}
